50398bacbc0eae87f1c12ca1be344504c39fc632
jayasankar-shyam <jayasankar06shyam@gmail.com>
2022-12-10 13:06:43 +0530

    added widgets

--- login.py
+++ login.py
@@ -6,9 +6,20 @@ win.title("My Library")
 win.geometry('340x440')
 win.configure(bg='#333333')
 
-label=tk.Label(win,text="Login")
-#label.pack()
+#Creating widgets
+loginLabel=tk.Label(win,text="Login")
+userLabel=tk.Label(win,text="Username")
+passLabel=tk.Label(win,text="Password")
+userInput=tk.Entry(win)
+passInput=tk.Entry(win,show="*")
+loginButton=tk.Button(win,text="Login")
 
-
+#placing on screen
+loginLabel.grid(row=0,column=0,columnspan=3)
+userLabel.grid(row=1,column=0)
+userInput.grid(row=1,column=1)
+passLabel.grid(row=2,column=0)
+passInput.grid(row=2,column=1)
+loginButton.grid(row=3,column=0,columnspan=3)
 
 win.mainloop()
