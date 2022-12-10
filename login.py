38c32b6361b20e0e88472a37197d2d585ac79d49
jayasankar-shyam <jayasankar06shyam@gmail.com>
2022-12-10 13:48:39 +0530

    added messagebox

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 #window specs
 bg="#333333"
@@ -7,15 +8,23 @@ win=tk.Tk()
 win.title("My Library")
 win.geometry('340x440')
 win.configure(bg=bg)
-
 frame=tk.Frame(bg=bg)
+
+def login():
+    username="test"
+    password="pass"
+    if userInput.get()==username and passInput.get()==password:
+        messagebox.showinfo(title="Login Successfull !!",message="You have successfully logged in.")
+    else:
+        messagebox.showinfo(title="Login Failed !!",message="Please check your credentials.")
+
 #Creating widgets
 loginLabel=tk.Label(frame,text="Login",bg=bg,fg=fg,font=("Arial",30))
 userLabel=tk.Label(frame,text="Username",bg=bg,fg=fg,font=("Arial",16))
 passLabel=tk.Label(frame,text="Password",bg=bg,fg=fg,font=("Arial",16))
 userInput=tk.Entry(frame,font=("Arial",16))
 passInput=tk.Entry(frame,show="*",font=("Arial",16))
-loginButton=tk.Button(frame,text="Login",fg=fg,bg="#f35e46",font=("Arial",16))
+loginButton=tk.Button(frame,text="Login",fg=fg,bg="#f35e46",font=("Arial",16),command=login)
 
 #placing on screen
 loginLabel.grid(row=0,column=0,columnspan=2,sticky="news",pady=40)
